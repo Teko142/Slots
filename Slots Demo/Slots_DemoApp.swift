62f2753287ded4a2080cbc572fda7984dd6cd8b4
Arthur <olehno.arturs@gmail.com>
2022-11-02 00:11:48 +0200

    1.0.1

---
+++ Slots Demo/Slots_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slots_DemoApp.swift
+//  Slots Demo
+//
+//  Created by Артур Олехно on 31/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Slots_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
